1496717af407f36be1aa5a86213126241e2b8559
Kimhyeyun <cqqwer@gmail.com>
2021-09-12 19:18:10 +0900

    Weekly 6 Solved

--- PROGRAMMERS/WeeklyChallenge/Main_P_3주차_퍼즐조각채우기.java
+++ PROGRAMMERS/WeeklyChallenge/Main_P_3주차_퍼즐조각채우기.java
@@ -1,7 +1,9 @@
 public class Main_P_3주차_퍼즐조각채우기 {
     
-    public int solution(int[][] game_board, int[][] table) {
+    public static int solution(int[][] game_board, int[][] table) {
         int answer = -1;
+
+     
         return answer;
     }
 }
